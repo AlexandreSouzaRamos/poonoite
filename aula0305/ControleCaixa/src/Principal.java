d208855871471c7ba8dd9da84126c207d70c0ada
maromo71 <professormoraes@gmail.com>
2022-05-03 22:07:24 -0300

    Atualizacao do codigo controle de caixa

--- aula0305/ControleCaixa/src/Principal.java
+++ aula0305/ControleCaixa/src/Principal.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 
 public class Principal {
     public static void main(String[] args) {
-        TelaMovimento tela  = new TelaMovimento();
-        tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        tela.setVisible(true);
+        JFrame tela2 = new JFrame();
+        tela2.setContentPane(new Tela2().getPanelTela());
+        tela2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        tela2.setSize(350,400);
+        tela2.setTitle("Controle de Caixa");
+        tela2.setVisible(true);
     }
 }
---
+++ aula0305/ControleCaixa/src/Tela2.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Tela2 {
+    private JPanel panelTela;
+    private JTextField txtValor;
+    private JTextField txtSaldo;
+    private JButton cmdEntrada;
+    private JButton cmdRetirada;
+    private JButton cmdConsulta;
+    private JButton cmdSair;
+    private JTextArea txtMsg;
+
+    public JPanel getPanelTela() {
+        return panelTela;
+    }
+    private Caixa caixa;
+
+    public Tela2(){
+        caixa = new Caixa();
+
+        cmdSair.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+        cmdEntrada.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                double valor =Double.parseDouble(txtValor.getText());
+                caixa.depositar(valor);
+                String frase = "Depósito de " + valor + " efetuado.";
+                txtMsg.append(frase + "\n");
+                txtValor.setText(null);
+                txtValor.requestFocus();
+                JOptionPane.showMessageDialog(null,
+                        "Depósito efetuado com sucesso",
+                        "Depósito - Sucesso",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        cmdRetirada.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                double valor = Double.parseDouble(txtValor.getText());
+                caixa.sacar(valor);
+                String frase = "Saque de " + valor + " efetuado.";
+                txtMsg.append(frase + "\n");
+                txtValor.setText(null);
+                txtValor.requestFocus();
+            }
+        });
+        cmdConsulta.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                double saldo = caixa.getSaldo();
+                txtSaldo.setText(Double.toString(saldo));
+                String frase = "Consulta efetuada";
+                txtMsg.append(frase + "\n");
+                txtValor.setText(null);
+                txtValor.requestFocus();
+            }
+        });
+    }
+}
